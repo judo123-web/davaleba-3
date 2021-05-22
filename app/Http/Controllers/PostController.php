4b7ce408a87292b89a3f243bda51dda523bb8bee
judo123-web <nika.kopadze.1@btu.edu.ge>
2021-05-22 16:38:42 +0400

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index() {
+        $posts = Post::all();
+        return view('posts.index',compact('posts'));
+    }
+
+    public function show($id) {
+        $post = Post::findorfail($id);
+        return view('posts.show',compact('post'));
+    }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(Request $request) {
+        Post::create([
+            'first_name' => $request->get('first_name'),
+            'last_name' => $request->get('last_name'),
+            'age' => $request->get('age'),
+            'post_text' => $request->get('post_text'),
+
+
+        ]);
+        return redirect()->back();
+    }
+
+    public function edit ($id){
+        $post = Post::findorfail($id);
+        return view('posts.edit',compact('post'));
+    }
+
+    public function update(Request $request,$id){
+        $post = Post::findorfail($id);
+        $post->update($request->all());
+        return redirect()->back();
+
+    }
+
+    public function delete($id){
+        $post = Post::findorfail($id);
+        $post->delete();
+        return redirect()->back();
+    }
+
+
+}
